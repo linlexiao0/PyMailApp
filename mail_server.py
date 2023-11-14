18f7ea12118e076f2d0f30de700ef4d2e5e87ebc
Lin Lexiao <2623459403@qq.com>
2023-11-14 16:15:26 +0800

    111

--- mail_server.py
+++ mail_server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, world! 16:11"
+    return "Hello, world! new new new ver"
 
 @app.route('/send',methods=['post'])
 def send():
